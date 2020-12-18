3354cb47193b1c74732e63d714ad9199efad2b45
Lukas Bombach <lukasbombach@googlemail.com>
2020-12-18 21:58:43 +0100

    simplified first test cases

--- packages/test/src/bluetooth.test.ts
+++ packages/test/src/bluetooth.test.ts
@@ -3,21 +3,16 @@ import { NodeBluetoothDevice } from "@web-bluetooth-node/test-adapter";
 import type { NodeRequestDeviceOptions } from "@web-bluetooth-node/types";
 
 describe("requestDevice", () => {
-  // TODO generate brute force options
-  test.each<NodeRequestDeviceOptions | undefined>([
-    undefined,
-    { filters: [] },
-    { filters: [{ name: "name" }] },
-    { filters: [], optionalServices: [] },
-    { filters: [], optionalServices: [1] },
-    { filters: [{ name: "name" }], optionalServices: [1] },
-    { filters: [{ name: "name" }], optionalServices: ["1"] },
-  ])("the params %s return the expected device", async options => {
-    const device = await bluetooth.requestDevice(options, () => true);
-    const name = options?.filters?.length ? options.filters[0].name : undefined;
-    const uuids = options?.optionalServices?.map(s => s.toString());
-    expect(device).toBeInstanceOf(NodeBluetoothDevice);
-    expect(device.name).toBe(name);
-    expect(device.uuids).toEqual(uuids);
-  });
+  test.each<NodeRequestDeviceOptions | undefined>([undefined])(
+    "the params %s return the expected device",
+    async options => {
+      const device = await bluetooth.requestDevice(options, () => true);
+      const filters = options?.filters || [];
+      const name = filters.length ? filters[0].name : undefined;
+      const uuids = options?.optionalServices?.map(s => s.toString());
+      expect(device).toBeInstanceOf(NodeBluetoothDevice);
+      expect(device.name).toBe(name);
+      expect(device.uuids).toEqual(uuids);
+    }
+  );
 });
