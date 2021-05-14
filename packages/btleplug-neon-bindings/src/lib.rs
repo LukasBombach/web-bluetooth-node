c5aabcceaca9ae14a39400772d48aa68bc70e92c
Lukas Bombach <lukas.bombach@googlemail.com>
2021-05-14 22:13:57 +0200

    created struct

--- packages/btleplug-neon-bindings/src/lib.rs
+++ packages/btleplug-neon-bindings/src/lib.rs
@@ -12,16 +12,6 @@ use btleplug::corebluetooth::{adapter::Adapter, manager::Manager};
 #[cfg(target_os = "windows")]
 use btleplug::winrtble::{adapter::Adapter, manager::Manager};
 
-// adapter retrieval works differently depending on your platform right now.
-// API needs to be aligned.
-
-fn get_central(manager: &Manager) -> Adapter {
-    let adapters = manager.adapters().unwrap();
-    adapters.into_iter().nth(0).unwrap()
-}
-
-
-
 pub struct BleCentral {
     adapter: Adapter,
     event_receiver: Receiver<CentralEvent>,
@@ -31,8 +21,8 @@ impl BleCentral {
     fn new() -> Self {
         let manager = Manager::new().unwrap();
         let adapters = manager.adapters().unwrap();
-        let adapter = adapters.into_iter().nth(0).unwrap(),
-        let event_receiver = adapter.event_receiver().unwrap(),
+        let adapter = adapters.into_iter().nth(0).unwrap();
+        let event_receiver = adapter.event_receiver().unwrap();
 
         BleCentral {
             adapter: adapter,
@@ -42,79 +32,57 @@ impl BleCentral {
 
     fn start_scan(&self) -> () {
         self.adapter.start_scan().unwrap();
+
+        while let Ok(event) = self.event_receiver.recv() {
+            match event {
+                CentralEvent::DeviceDiscovered(bd_addr) => {
+                    println!("DeviceDiscovered: {:?}", bd_addr);
+                }
+                CentralEvent::DeviceConnected(bd_addr) => {
+                    println!("DeviceConnected: {:?}", bd_addr);
+                }
+                CentralEvent::DeviceDisconnected(bd_addr) => {
+                    println!("DeviceDisconnected: {:?}", bd_addr);
+                }
+                CentralEvent::ManufacturerDataAdvertisement {
+                    address,
+                    manufacturer_id,
+                    data,
+                } => {
+                    println!(
+                        "ManufacturerDataAdvertisement: {:?}, {}, {:?}",
+                        address, manufacturer_id, data
+                    );
+                }
+                CentralEvent::ServiceDataAdvertisement {
+                    address,
+                    service,
+                    data,
+                } => {
+                    println!(
+                        "ServiceDataAdvertisement: {:?}, {}, {:?}",
+                        address,
+                        service.to_short_string(),
+                        data
+                    );
+                }
+                CentralEvent::ServicesAdvertisement { address, services } => {
+                    let services: Vec<String> =
+                        services.into_iter().map(|s| s.to_short_string()).collect();
+                    println!("ServicesAdvertisement: {:?}, {:?}", address, services);
+                }
+                _ => {}
+            }
+        }
     }
 }
 
 
 fn ble(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let manager = Manager::new().unwrap();
-
-    // get the first bluetooth adapter
-    // connect to the adapter
-    let central = get_central(&manager);
-
-    // Each adapter can only have one event receiver. We fetch it via
-    // event_receiver(), which will return an option. The first time the getter
-    // is called, it will return Some(Receiver<CentralEvent>). After that, it
-    // will only return None.
-    //
-    // While this API is awkward, is is done as not to disrupt the adapter
-    // retrieval system in btleplug v0.x while still allowing us to use event
-    // streams/channels instead of callbacks. In btleplug v1.x, we'll retrieve
-    // channels as part of adapter construction.
-    let event_receiver = central.event_receiver().unwrap();
-
-    // start scanning for devices
-    central.start_scan().unwrap();
-
-    // Print based on whatever the event receiver outputs. Note that the event
-    // receiver blocks, so in a real program, this should be run in its own
-    // thread (not task, as this library does not yet use async channels).
-    while let Ok(event) = event_receiver.recv() {
-        match event {
-            CentralEvent::DeviceDiscovered(bd_addr) => {
-                println!("DeviceDiscovered: {:?}", bd_addr);
-            }
-            CentralEvent::DeviceConnected(bd_addr) => {
-                println!("DeviceConnected: {:?}", bd_addr);
-            }
-            CentralEvent::DeviceDisconnected(bd_addr) => {
-                println!("DeviceDisconnected: {:?}", bd_addr);
-            }
-            CentralEvent::ManufacturerDataAdvertisement {
-                address,
-                manufacturer_id,
-                data,
-            } => {
-                println!(
-                    "ManufacturerDataAdvertisement: {:?}, {}, {:?}",
-                    address, manufacturer_id, data
-                );
-            }
-            CentralEvent::ServiceDataAdvertisement {
-                address,
-                service,
-                data,
-            } => {
-                println!(
-                    "ServiceDataAdvertisement: {:?}, {}, {:?}",
-                    address,
-                    service.to_short_string(),
-                    data
-                );
-            }
-            CentralEvent::ServicesAdvertisement { address, services } => {
-                let services: Vec<String> =
-                    services.into_iter().map(|s| s.to_short_string()).collect();
-                println!("ServicesAdvertisement: {:?}, {:?}", address, services);
-            }
-            _ => {}
-        }
-    }
-      Ok(cx.undefined())
+    let central = BleCentral::new();
+    central.start_scan();
+    Ok(cx.undefined())
 }
-
-
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
