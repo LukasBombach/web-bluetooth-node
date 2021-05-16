94c5f12ec0a32baee721599a695a376ecd0889c0
Lukas Bombach <lukas.bombach@googlemail.com>
2021-05-16 21:12:56 +0200

    failed

--- packages/btleplug-neon-bindings/index.js
+++ packages/btleplug-neon-bindings/index.js
@@ -1,3 +1,27 @@
-const { ble } = require(".");
+/* const { person_new, person_set_name, person_greet } = require(".");
 
-console.log(ble());
+class Person {
+  constructor(firstName, lastName) {
+    this.boxed = person_new(firstName, lastName);
+  }
+
+  setName(name) {
+    // person_set_name(this.boxed, name);
+  }
+
+  greet() {
+    // person_greet(this.boxed);
+    this.boxed.greet();
+  }
+}
+
+console.log("Creating a person");
+const person = new Person("Eddie", "Van Halen");
+
+console.log("Greeting");
+console.log(person.greet());
+ */
+
+const addon = require(".");
+
+console.log("xyz");
